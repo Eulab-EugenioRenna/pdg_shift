b550440bad243f5797921583f5a56a33ab7db964
Eugenio Renna <rennaeugenio92@gmail.com>
2025-07-09 20:31:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-cen

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,9 +3,12 @@ export function Footer() {
 
   return (
     <footer className="py-6 md:px-8 md:py-0 bg-secondary/50">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
           © {currentYear} Grace Services. Tutti i diritti riservati.
+        </p>
+        <p className="text-center text-sm leading-loose text-muted-foreground md:text-right">
+          Made by Eulab with ❤️ for Parola della Grazia
         </p>
       </div>
     </footer>
