847820e4645fcf539a7a3a7865968e42275e9977
Eugenio Renna <rennaeugenio92@gmail.com>
2025-07-10 10:49:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export const pb = new PocketBase('https://pocketbase.eulab.cloud');
-pb.autoCancellation = false;
+pb
 
 // This is a helper to easily check auth status.
 // It's reactive, so it will update automatically.
