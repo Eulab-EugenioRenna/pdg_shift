beed3d40cbc19cdf8c833975681cceb091109b55
Eugenio Renna <rennaeugenio92@gmail.com>
2025-07-10 10:37:35 +0000

    set pocketbase autocancell false

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export const pb = new PocketBase('https://pocketbase.eulab.cloud');
+pb.autoCancellation = false;
 
 // This is a helper to easily check auth status.
 // It's reactive, so it will update automatically.
